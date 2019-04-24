d1b26ffc353c23efa586b13d8b1d5b3d3027085e
Andrew Heekin <andrewheekin@gmail.com>
2019-04-24 11:15:40 -0400

    react-native link instabug-reactnative

--- android/app/src/main/java/com/instabugtester/MainApplication.java
+++ android/app/src/main/java/com/instabugtester/MainApplication.java
@@ -3,6 +3,7 @@ package com.instabugtester;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.instabug.reactlibrary.RNInstabugReactnativePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            		new RNInstabugReactnativePackage.Builder("YOUR_ANDROID_APPLICATION_TOKEN",MainApplication.this)
+							.setInvocationEvent("shake")
+							.setPrimaryColor("#1D82DC")
+							.setFloatingEdge("left")
+							.setFloatingButtonOffsetFromTop(250)
+							.build()
       );
     }
 
